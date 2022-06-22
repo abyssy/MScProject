e8197242178b00c7d634cbcbf230d2a244f6b8cc
yangqixuan <2645960y@student.glasgow.ac.uk>
2022-06-22 18:20:06 +0100

    finished the reduction algorithm from SPA_SLQp to SPA_S

---
+++ ReadInstance.py
@@ -0,0 +1,89 @@
+class ReadInstance:
+    def __init__(self):
+        self.students = 0  # keeps track of number of students
+        self.projects = 0  # keeps track of number of projects
+        self.lecturers = 0  # keeps track of number of lecturers
+        self.sp = dict()  # {student: [ordered_preference_list, dictionary pointing each project to its rank]}
+        self.plc = dict()  # {project: [lecturer, project_capacity, project_lower_quotas]}
+        self.lp = dict()  # {lecturer; [lecturer_capacity, ordered_preference_list_Lk, dictionary pointing each project to Lkj]}
+        self.lp_rank = dict()  # {lecturer: a dictionary pointing each student to her rank in Lk}
+        self.proj_rank = dict()  # {project: a dictionary pointing each student to her rank in Lkj}
+
+    # reads the SPA-SLQp instance
+    def read_file(self, filename):
+        with open(filename) as t:
+            t = t.readlines()
+        entry1 = t[0].rstrip(' \n').split(' ')
+        # entry1 = list(map(int, entry1)) #converts each element to an integer
+        self.students, self.projects, self.lecturers = int(entry1[0]), int(entry1[1]), int(entry1[2])
+
+        # -------------------------------------------------------------------------------------------------------------------
+        #  we build the student's dictionary
+
+        for i in range(1, self.students + 1):
+            entry = t[i].rstrip(' \n').split(' ')
+            student = 's' + str(entry[0])
+            preferencelist = ['p' + str(k) for k in entry[1:]]
+            length = len(preferencelist)
+            rank = {preferencelist[i]: i for i in
+                    range(length)}  # store the index of each project on each student's preference list
+            self.sp[student] = [preferencelist, rank]
+            # -------------------------------------------------------------------------------------------------------------------
+
+        # -------------------------------------------------------------------------------------------------------------------
+        #  we build the projects's dictionary
+
+        for i in range(self.students + 1, self.students + 1 + self.projects):
+            entry = t[i].rstrip(' \n').split(' ')
+            # project = [lecturer, project_capacity_yet_to_be_filled, full(project) = False, keep track of students that was rejected from project]
+            # length of the preferred students for p_j according to l_k to be appended when we have more information..
+            self.plc['p' + str(entry[0])] = ['l' + str(entry[3]), int(entry[1]), int(entry[2])]
+        # -------------------------------------------------------------------------------------------------------------------
+
+        # -------------------------------------------------------------------------------------------------------------------
+        #  we build the lecturer's dictionary
+
+        for i in range(self.students + 1 + self.projects, self.students + 1 + self.projects + self.lecturers):
+            entry = t[i].rstrip(' \n').split(' ')
+            lecturer = 'l' + str(entry[0])
+
+            lecturerpreferencelist = ['s' + str(k) for k in entry[2:]]
+            length = len(lecturerpreferencelist)
+            self.lp_rank[lecturer] = {lecturerpreferencelist[i]: i for i in
+                                      range(length)}  # stores rank of each student in L_k
+
+            # -------------------------------------------------------------------------------------------------------------------
+            #  another useful dictionary is created here and attached to the lecturer's dictionary - L_k_j
+            #  the lecturer's ranked preference list according to each project they offer
+            d = {}
+            for project in self.plc:
+                if self.plc[project][0] == lecturer:
+                    d[project] = []
+                    for student in lecturerpreferencelist:
+                        if project in self.sp[student][0]:
+                            d[project].append(student)
+                    length = len(d[project])
+                    self.proj_rank[project] = {d[project][i]: i for i in
+                                               range(length)}  # stores rank of each student in L_k_j
+
+            length = len(lecturerpreferencelist)
+            # lecturer = [lecturer_capacity, lecturerpreferencelist, d]
+            self.lp[lecturer] = [int(entry[1]), lecturerpreferencelist, d]
+            # -------------------------------------------------------------------------------------------------------------------------------
+        # -------------------------------------------------------------------------------------------------------------------------------
+
+
+s = ReadInstance()
+filename = "input.txt"
+s.read_file(filename)
+print("Student_Project:")
+print(s.sp)
+print("Project_lecturer:")
+print(s.plc)
+print("Lecturer_project:")
+print(s.lp)
+print("Lecturer_project_rank:")
+print(s.lp_rank)
+print("project_rank:")
+print(s.proj_rank)
+print()
---
+++ Reduction.py
@@ -0,0 +1,134 @@
+from ReadInstance import ReadInstance
+
+
+class Reduction:
+    def __init__(self, filename):
+        self.filename = filename
+        r = ReadInstance()
+        r.read_file(self.filename)
+
+        self.students = r.students
+        self.projects = r.projects
+        self.lecturers = r.lecturers
+
+        self.sp = dict()
+        self.plc = dict()
+        self.lp = dict()
+        self.lp_rank = dict()
+        self.proj_rank = dict()
+        self.p_status = dict()
+        # 1 : quota == 0
+        # 2 : quota > 0, capacity == quota
+        # 3 : quota > 0, capacity > quota
+        self.l_status = dict()
+
+        for i in range(self.projects):
+            project = 'p' + str(i + 1)
+            lecturer = r.plc[project][0]
+            capacity = r.plc[project][1]
+            quota = r.plc[project][2]
+            if quota > 0:
+                self.p_status[project] = 2
+                self.l_status[lecturer] = 2
+                self.plc[project + '2'] = [lecturer + '2', quota]
+                if capacity - quota > 0:
+                    self.p_status[project] = 3
+                    self.plc[project + '1'] = [lecturer + '1', capacity - quota]
+            else:
+                self.p_status[project] = 1
+                self.plc[project + '1'] = [lecturer + '1', capacity]
+
+        for i in range(self.students):
+            student = 's' + str(i + 1)
+            preference_list = []
+            for project in r.sp[student][0]:
+                if self.p_status[project] == 1:
+                    preference_list.append(project + '1')
+                elif self.p_status[project] == 2:
+                    preference_list.append(project + '2')
+                elif self.p_status[project] == 3:
+                    preference_list.append(project + '2')
+                    preference_list.append(project + '1')
+            length = len(preference_list)
+            rank = {preference_list[i]: i for i in range(length)}
+            self.sp[student] = [preference_list, rank]
+
+        for i in range(self.lecturers):
+            lecturer = 'l' + str(i + 1)
+            lecturer_preference_list = []
+            # print(r.lp[lecturer][1])
+            if self.l_status.get(lecturer) == 2:
+                capacity = 0
+                old_capacity = r.lp[lecturer][0]
+                for student in r.lp[lecturer][1]:
+                    for project in self.sp[student][0]:
+                        if self.plc[project][0][0:2] == lecturer and self.plc[project][0][2] == '2':
+                            lecturer_preference_list.append(student)
+                            break
+                length = len(lecturer_preference_list)
+                self.lp_rank[lecturer + '2'] = {lecturer_preference_list[i]: i for i in range(length)}
+
+                for project in self.plc:
+                    if self.plc[project][0][0:2] == lecturer and self.plc[project][0][2] == '2':
+                        capacity += self.plc[project][1]
+                self.lp[lecturer + '2'] = [capacity, self.lp_rank[lecturer + '2']]
+
+                d = {}
+                for project in self.plc:
+                    if self.plc[project][0] == lecturer + '2':
+                        d[project] = []
+                        for student in lecturer_preference_list:
+                            if project in self.sp[student][0]:
+                                d[project].append(student)
+                        length = len(d[project])
+                        self.proj_rank[project] = {d[project][i]: i for i in range(length)}
+
+                if old_capacity - capacity > 0:
+                    lecturer_preference_list = r.lp[lecturer][1]
+                    length = len(lecturer_preference_list)
+                    self.lp_rank[lecturer + '1'] = {lecturer_preference_list[i]: i for i in range(length)}
+                    self.lp[lecturer + '1'] = [old_capacity - capacity, self.lp_rank[lecturer + '1']]
+
+                    d = {}
+                    for project in self.plc:
+                        if self.plc[project][0] == lecturer + '1':
+                            d[project] = []
+                            for student in lecturer_preference_list:
+                                if project in self.sp[student][0]:
+                                    d[project].append(student)
+                            length = len(d[project])
+                            self.proj_rank[project] = {d[project][i]: i for i in range(length)}
+                else:
+                    for project in list(self.plc.keys()):
+                        if self.plc[project][0] == lecturer + '1':
+                            self.plc.pop(project)
+                            for student in self.sp:
+                                if project in self.sp[student][0]:
+                                    self.sp[student][0].remove(project)
+                                    self.sp[student][1].pop(project)
+            else:
+                capacity = r.lp[lecturer][0]
+                lecturer_preference_list = r.lp[lecturer][1]
+                length = len(lecturer_preference_list)
+                self.lp_rank[lecturer + '1'] = {lecturer_preference_list[i]: i for i in range(length)}
+                self.lp[lecturer + '1'] = [capacity, self.lp_rank[lecturer + '1']]
+
+        for student in self.sp:
+            preference_list = self.sp[student][0]
+            length = len(preference_list)
+            rank = {preference_list[i]: i for i in range(length)}
+            self.sp[student] = [preference_list, rank]
+
+
+filename = "input.txt"
+r = Reduction(filename)
+print("Student_Project:")
+print(r.sp)
+print("Project_lecturer:")
+print(r.plc)
+print("Lecturer_project:")
+print(r.lp)
+print("Lecturer_project_rank:")
+print(r.lp_rank)
+print("project_rank:")
+print(r.proj_rank)
